8a56af04e2159075eed309b62067bd1980eeab92
zhtaa <734040900@qq.com>
2019-02-18 10:33:45 +0800

    Picture-Browsing

---
+++ PictureFrom/PictureFrom/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PictureFrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button button4;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listbox1 = new System.Windows.Forms.ListBox();
+            button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            button4.Location = new System.Drawing.Point(504, 32);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(52, 35);
+            button4.TabIndex = 6;
+            button4.Text = "Next_page";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(50, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OpenDir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(270, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Location = new System.Drawing.Point(808, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(40, 73);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(633, 502);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(120, 120);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 32);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(367, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0/n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(562, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 33);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0/n";
+            // 
+            // listbox1
+            // 
+            this.listbox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listbox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listbox1.FormattingEnabled = true;
+            this.listbox1.HorizontalScrollbar = true;
+            this.listbox1.ItemHeight = 12;
+            this.listbox1.Location = new System.Drawing.Point(679, 67);
+            this.listbox1.Name = "listbox1";
+            this.listbox1.Size = new System.Drawing.Size(185, 508);
+            this.listbox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 620);
+            this.Controls.Add(this.listbox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listbox1;
+    }
+}
+
---
+++ PictureFrom/PictureFrom/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureFrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath;
+        private string foldPath;
+        private string picturePath = null;
+        private List<string> imagePathList = new List<string>();
+        private int index =0;
+
+        //private int current_file_size = 0;
+        private int next_page_index = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.foldPath = folderBrowserDialog1.SelectedPath;
+                    
+                    showPicture();
+                    listbox1.Items.Clear();
+                }
+                else if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                {
+                    MessageBox.Show("取消显示图片列表");
+                }
+            }
+            catch (Exception msg)
+            {
+                throw msg;
+            }
+        }
+
+        private void showPicture()
+        {
+            
+           
+            DirectoryInfo dic = new DirectoryInfo(foldPath);
+            DirectoryInfo[] foldInfo = dic.GetDirectories();
+            if (index>=foldInfo.Length)
+            {
+                index = foldInfo.Length - 1;
+                MessageBox.Show("Final Picture");
+            }
+            filePath = foldInfo[index].FullName;
+            label1.Text = index.ToString()+"\\"+foldInfo.Length;
+            DirectoryInfo dir = new DirectoryInfo(filePath);
+            FileInfo[] fileInfo = dir.GetFiles("*.jpg");
+            for (int i = 0; i < fileInfo.Length; i++)
+            {
+                picturePath = fileInfo[i].FullName;
+                imagePathList.Add(picturePath);
+                
+                this.imageList1.Images.Add(Image.FromFile(picturePath));
+            }
+            //this.imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            //imageList1.ImageSize = new Size(200, 200);
+            //this.listView1.Items.Clear();
+            this.listView1.LargeImageList = this.imageList1;
+           // this.listView1.View = View.LargeIcon;
+            Loadpicture();
+        }
+
+
+            //current_file_size = fileInfo.Length;
+
+            
+        private void Loadpicture()
+            {
+            int totle = imageList1.Images.Count / 50;
+            label2.Text = next_page_index.ToString() + "\\" + totle.ToString();
+            this.listView1.Items.Clear();
+            this.listView1.BeginUpdate();
+            int start = next_page_index * 50;
+            int end = next_page_index * 50 + 50;
+            if (end > imageList1.Images.Count)
+            {
+                end = imageList1.Images.Count;
+            }
+
+            //if (start == end)
+            //{
+            //    return;
+            //}
+
+            for (int i = start;i< end;i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.ImageIndex = i;
+                this.listView1.Items.Add(lvi);
+            }
+            this.listView1.EndUpdate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            next_page_index = 0;
+            this.listView1.Items.Clear();
+            this.imageList1.Images.Clear();
+            Console.WriteLine(foldPath.Length);
+            if (textBox1.Text.Trim() == string.Empty)
+            {
+                index++;
+            }
+            else if (IsNumber(textBox1.Text.Trim()) == false)
+            {
+                MessageBox.Show("文本框里只能为数字");
+            }         
+            else
+            {
+                index = Convert.ToInt32(textBox1.Text.Trim());
+            }
+            showPicture();
+
+
+        }
+
+        private bool IsNumber(string oText)
+        {
+            try
+            {
+                int var1 = Convert.ToInt32(oText);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("‪C:\\Users\\admin\\Desktop\\PicturePath.txt"))
+            {
+                FileStream fs1 = new FileStream("C:\\Users\\admin\\Desktop\\PicturePath.txt", FileMode.Append, FileAccess.Write);//创建写入文件 
+                StreamWriter sw = new StreamWriter(fs1);
+                sw.WriteLine(filePath.ToString());//开始写入值
+                sw.Close();
+                fs1.Close();
+                listbox1.Items.Add(filePath.ToString());
+            }
+            else
+            {
+                FileStream fs = new FileStream("C:\\Users\\admin\\Desktop\\PicturePath.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(filePath.ToString());//开始写入值
+                sr.Close();
+                fs.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            next_page_index++;
+            Loadpicture();
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((Keys)e.KeyChar == Keys.Q)
+            {
+                button2_Click(sender, e);
+            }
+            if ((Keys)e.KeyChar == Keys.E)
+            {
+                button4_Click(sender, e);
+            }
+            if ((Keys)e.KeyChar == Keys.S)
+                button3_Click(sender, e);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((Keys)e.KeyValue== Keys.Q)
+            {
+                button2_Click(sender, e);
+            }
+            if ((Keys)e.KeyValue == Keys.E)
+            {
+                button4_Click(sender, e);
+            }
+            if ((Keys)e.KeyValue == Keys.S)
+                button3_Click(sender, e);
+
+        }
+    }
+}
+
